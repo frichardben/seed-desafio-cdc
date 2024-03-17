ece1eaf6de83c4e74a88cc6a2668b203349a5385
Richard Ben <frichardben@yahoo.com.br>
2024-03-17 18:06:32 -0300

    feat(author): created migrate

---
+++ src/infra/typeorm/migration/1710699320439-create_table_authors.ts
@@ -0,0 +1,39 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateTableAuthors1710699320439 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'authors',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'vachar',
+          },
+          {
+            name: 'email',
+            type: 'vachar',
+          },
+          {
+            name: 'description',
+            type: 'vachar',
+          },
+          {
+            name: 'register',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.dropTable('authors');
+  }
+}
