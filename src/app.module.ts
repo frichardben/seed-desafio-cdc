93e39c7a12fa12a10902359883de011ec069b41e
Richard Ben <frichardben@yahoo.com.br>
2024-03-17 18:03:14 -0300

    chore(typeorm): config sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { AuthorModule } from './modules/author/author.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: ':memory:',
+      entities: [__dirname + '/**/*.entity{.js,.ts}'],
+      migrations: [__dirname, '../../infra/typeorm/migration/{.ts,.js}'],
+      synchronize: true,
+    }),
+    AuthorModule,
+  ],
   controllers: [],
   providers: [],
 })
