420c3ee4dac9e13d4c727eb4e6aca85bffdd0a7a
Richard Ben <frichardben@yahoo.com.br>
2024-03-24 18:56:51 -0300

    chore(authorDTO): added message

--- src/modules/author/dto/create-author.dto.ts
+++ src/modules/author/dto/create-author.dto.ts
@@ -4,13 +4,15 @@ export class CreateAuthorDto {
   @IsNotEmpty({ message: 'Name is required' })
   name: string;
 
-  @IsEmail()
+  @IsEmail({}, { message: 'Email is invalid' })
   @IsNotEmpty({ message: 'Email is required' })
   @isUnique({ tableName: 'authors', column: 'email' })
   email: string;
 
   @IsNotEmpty({ message: 'Description is required' })
-  @MaxLength(400)
+  @MaxLength(400, {
+    message: 'Description cannot be longer than 400 characters.',
+  })
   description: string;
 
   register: Date;
