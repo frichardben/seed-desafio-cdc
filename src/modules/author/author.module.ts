b716a0df616fd4e1d1d8f63ad503d9f0285bc4af
Richard Ben <frichardben@yahoo.com.br>
2024-04-25 08:07:13 -0300

    chore(author): custom providers

--- src/modules/author/author.module.ts
+++ src/modules/author/author.module.ts
@@ -3,12 +3,16 @@ import { AuthorService } from './author.service';
 import { AuthorController } from './author.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Author } from './entities/author.entity';
+import { AuthorRepository } from './repositories/author.repository';
 import { TypeOrmAuthorRepository } from './repositories/typeorm/typeorm.author.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Author])],
   controllers: [AuthorController],
-  providers: [AuthorService, TypeOrmAuthorRepository],
+  providers: [
+    AuthorService,
+    { provide: AuthorRepository, useClass: TypeOrmAuthorRepository },
+  ],
   exports: [AuthorService],
 })
 export class AuthorModule {}
