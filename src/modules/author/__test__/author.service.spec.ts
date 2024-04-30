49c4e1dd35baa1c47d94966166c2f56d318e322a
Richard Ben <frichardben@yahoo.com.br>
2024-04-29 21:23:55 -0300

    chore(author): remove TestingModule

--- src/modules/author/__test__/author.service.spec.ts
+++ src/modules/author/__test__/author.service.spec.ts
@@ -1,31 +1,17 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { AuthorService } from '../author.service';
 import { InMemoryAuthorRepository } from './repository/in-memory/in-memory.author.repository';
 import { CreateAuthorDto } from '../dto/create-author.dto';
 
-import { AuthorRepository } from '../repositories/author.repository';
 import { randomUUID } from 'crypto';
+import { NotFoundException } from '@nestjs/common';
 
 let service: AuthorService;
 let inMemoryAuthorRepository: InMemoryAuthorRepository;
 
 describe('AuthorService', () => {
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        AuthorService,
-        InMemoryAuthorRepository,
-        {
-          provide: AuthorRepository,
-          useClass: InMemoryAuthorRepository,
-        },
-      ],
-    }).compile();
-
-    inMemoryAuthorRepository = module.get<InMemoryAuthorRepository>(
-      InMemoryAuthorRepository,
-    );
-    service = module.get<AuthorService>(AuthorService);
+  beforeEach(() => {
+    inMemoryAuthorRepository = new InMemoryAuthorRepository();
+    service = new AuthorService(inMemoryAuthorRepository);
   });
 
   it('should be defined', () => {
@@ -40,8 +26,7 @@ describe('AuthorService', () => {
       register: new Date(),
     };
 
-    const createdAuthorInMemory =
-      await inMemoryAuthorRepository.store(authorData);
+    const createdAuthorInMemory = await service.create(authorData);
 
     expect(inMemoryAuthorRepository.items).toContainEqual(
       createdAuthorInMemory,
@@ -57,8 +42,6 @@ describe('AuthorService', () => {
     };
     await service.create(authorDataFirst);
 
-    await inMemoryAuthorRepository.store(authorDataFirst);
-
     const authorDataSecond: CreateAuthorDto = {
       name: 'Jane Doe',
       email: 'jane@example.com',
@@ -67,20 +50,15 @@ describe('AuthorService', () => {
     };
     await service.create(authorDataSecond);
 
-    await inMemoryAuthorRepository.store(authorDataSecond);
-
-    const allAuthors = await service.findAll();
-
-    expect(allAuthors).toHaveLength(2);
+    expect(inMemoryAuthorRepository.items).toHaveLength(2);
   });
 
   it('should not find author by id', async () => {
     const authorId = randomUUID();
 
-    const findAuthorInMemory =
-      await inMemoryAuthorRepository.findById(authorId);
+    const findAuthorInMemory = service.findById(authorId);
 
-    expect(findAuthorInMemory).toBeNull();
+    expect(findAuthorInMemory).rejects.toThrow(NotFoundException);
   });
 
   it('should  find author by id', async () => {
