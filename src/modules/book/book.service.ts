7125c6ee24199392d2e9680848f1a71aec3f1d6e
Richard Ben <frichardben@yahoo.com.br>
2024-08-31 15:29:22 -0300

    feat: update for repository

--- src/modules/book/book.service.ts
+++ src/modules/book/book.service.ts
@@ -1,54 +1,40 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateBookDto } from './dto/create-book.dto';
-import { UpdateBookDto } from './dto/update-book.dto';
-import { Book } from './entities/book.entity';
-import { Repository } from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
-import { instanceToPlain } from 'class-transformer';
 import { AuthorService } from '../author/author.service';
 import { CategoryService } from '../category/category.service';
+import { BookRepository } from '@/modules/book/repositories/book.repository';
+import { instanceToPlain } from "class-transformer";
 
 @Injectable()
 export class BookService {
   constructor(
-    @InjectRepository(Book)
-    private bookRepo: Repository<Book>,
+    private bookRepo: BookRepository,
     private authorService: AuthorService,
     private categoryService: CategoryService,
   ) {}
 
   async create(createBookDto: CreateBookDto) {
     await this.authorService.findById(createBookDto.authorId);
+
     await this.categoryService.findOne(createBookDto.categoryId);
 
-    return await this.bookRepo.save(createBookDto);
+    return await this.bookRepo.store(createBookDto);
   }
 
   async findAll() {
     const books = instanceToPlain(
-      await this.bookRepo.find({ select: ['id', 'title'] }),
+      await this.bookRepo.findAll(),
     );
-    return { books };
+    return books;
   }
 
   async findOne(id: string) {
-    const book = await this.bookRepo.findOne({
-      where: { id },
-      relations: ['author', 'category'],
-    });
+    const book = await this.bookRepo.findById(id);
 
     if (!book) {
       throw new NotFoundException('Book not found');
     }
 
     return book;
-  }
-
-  async update(id: number, updateBookDto: UpdateBookDto) {
-    return await this.bookRepo.update(id, updateBookDto);
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} book`;
   }
 }
