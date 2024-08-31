1d3aa850942057dbb5a6e9c1bd18e1621b57f0e1
Richard Ben <frichardben@yahoo.com.br>
2024-08-31 15:30:34 -0300

    chore: import bookRepository and typeOrmBookRepository

--- src/modules/book/book.module.ts
+++ src/modules/book/book.module.ts
@@ -5,10 +5,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Book } from './entities/book.entity';
 import { AuthorModule } from '../author/author.module';
 import { CategoryModule } from '../category/category.module';
+import { BookRepository } from './repositories/book.repository';
+import { TypeOrmBookRepository } from './repositories/typeorm/typeorm.book.repository';
+
 
 @Module({
   imports: [TypeOrmModule.forFeature([Book]), AuthorModule, CategoryModule],
   controllers: [BookController],
-  providers: [BookService],
+  providers: [
+    BookService,
+    { provide: BookRepository, useClass: TypeOrmBookRepository },
+  ],
+  exports: [BookService],
 })
 export class BookModule {}
