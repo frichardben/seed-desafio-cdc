d1010f3bcade8ce2ca4d1766b758488aa1a505ef
Richard Ben <frichardben@yahoo.com.br>
2024-10-19 15:46:04 -0300

    feat: created table countries

---
+++ src/infra/typeorm/migration/1725199621055-create_table_countries.ts
@@ -0,0 +1,26 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateTableCountries1725199621055 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'countries',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('countries');
+  }
+}
