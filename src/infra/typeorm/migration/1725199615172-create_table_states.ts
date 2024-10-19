e6743c001af2c1221cfbf200991427bd464a7bee
Richard Ben <frichardben@yahoo.com.br>
2024-10-19 15:57:57 -0300

    feat(state): created table

---
+++ src/infra/typeorm/migration/1725199615172-create_table_states.ts
@@ -0,0 +1,26 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateTableStates1725199615172 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'states',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('states');
+  }
+}
