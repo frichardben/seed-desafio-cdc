ed189381e706275cf621437f96d06dced838eb48
Richard Ben <frichardben@yahoo.com.br>
2025-03-02 20:56:17 -0300

    chore(postgres): config services

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,18 @@ import { IsUniqueConstraint } from './common/validation/UniqueConstraint/is-uniq
 import { BookModule } from './modules/book/book.module';
 import { CountryModule } from '@/modules/locale/country/country.module';
 import { StateModule } from '@/modules/locale/state/state.module';
+import { ConfigModule } from '@nestjs/config';
+import { PostgresConfigService } from '@/infra/config/postgres.config.service';
+
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: ':inmemory:',
-      entities: [__dirname + '/**/*.entity{.js,.ts}'],
-      migrations: [__dirname, '../../infra/typeorm/migration/{.ts,.js}'],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
     AuthorModule,
     CategoryModule,
